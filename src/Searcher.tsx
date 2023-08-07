2f3ef74fb4e374011ffc240152d207201ef1c05e
Natalia Ramirez Parra <jnataliaramirez@outlook.com>
2023-08-07 10:42:09 +0200

    Do Searcher component

---
+++ src/Searcher.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Searcher = () => {
+  return (
+    <section className="searcher-container">
+      <input type="search" id="organization" name="organization"></input>
+      <button type="button"> Buscar </button>
+    </section>
+  );
+};
--- src/list.tsx
+++ src/list.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Searcher } from "./Searcher";
 
 interface MemberEntity {
   id: string;
@@ -19,6 +20,9 @@ export const ListPage: React.FC = () => {
   return (
     <>
       <h2>Hello from List page</h2>
+
+      <Searcher />
+
       <div className="list-user-list-container">
         <span className="list-header">Avatar</span>
         <span className="list-header">Id</span>
