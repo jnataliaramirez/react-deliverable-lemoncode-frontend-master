5c355c5145cf0be8c7537e3e6697d5857c239607
Natalia Ramirez Parra <jnataliaramirez@outlook.com>
2023-08-09 13:16:07 +0200

    Add styles to Searcher component

--- src/Searcher.tsx
+++ src/Searcher.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { MyContext } from "./context";
+import { Button, TextField, Unstable_Grid2 } from "@mui/material";
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 
 export const Searcher = () => {
   const myContext = React.useContext(MyContext);
@@ -18,15 +20,25 @@ export const Searcher = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="searcher-container">
-      <input
-        type="search"
-        id="organization"
-        name="organization"
-        defaultValue={organization}
-        onKeyUp={handleKeypress}
-      />
-      <button type="submit">Buscar</button>
-    </form>
+    <Grid2 display="flex" justifyContent="center" marginTop="2rem" >
+      <form onSubmit={handleSubmit} className="searcher-container">
+        <TextField
+          id="organization"
+          label="Organización"
+          name="organization"
+          type="search"
+          defaultValue={organization}
+          onKeyUp={handleKeypress}
+          variant="outlined"
+          margin="none"
+          size="small"
+          color="primary"
+          sx={{ width: 200 }}
+        />
+        <Button type="submit" variant="outlined" sx={{}}>
+          Buscar
+        </Button>
+      </form>
+    </Grid2>
   );
 };
