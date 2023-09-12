fc15bcd68159706843146a5eacc43959d401871f
Natalia Ramirez Parra <jnataliaramirez@outlook.com>
2023-09-12 13:43:00 +0200

    Add MUI to FormOrder Component

--- challenge-order/src/components/FormOrder/index.tsx
+++ challenge-order/src/components/FormOrder/index.tsx
@@ -1,6 +1,7 @@
 import React, { ChangeEvent, FormEvent, useState } from "react";
 import { Button, TextField } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 
 interface FormData {
   number: string;
@@ -41,60 +42,77 @@ export const FormOrder = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <TextField
-        id="number"
-        label="Number:"
-        value={formData.number}
-        onChange={handleChange}
-        required
-        type="text"
-        variant="outlined"
-      />
-
-      <TextField
-        id="provider"
-        label="Provider:"
-        value={formData.provider}
-        onChange={handleChange}
-        required
-        type="text"
-        variant="outlined"
-      />
-      <TextField
-        id="date"
-        label="Date:"
-        value={formData.date}
-        onChange={handleChange}
-        required
-        type="date"
-        variant="outlined"
-      />
-      <TextField
-        id="totalAmount"
-        label="Total Amount:"
-        value={formData.totalAmount}
-        onChange={handleChange}
-        type="text"
-        variant="outlined"
-      />
-
-      <TextField
-        id="state"
-        label="State:"
-        value={formData.state}
-        onChange={handleChange}
-        type="text"
-        variant="outlined"
-      />
-
-      <Button
-        type="submit"
-        variant="contained"
-        size="medium"
-        endIcon={<SendIcon />}
+      <Grid2
+        display="flex"
+        flexDirection="column"
+        gap="1rem"
+        marginTop="1rem"
+        width="100%"
       >
-        Send
-      </Button>
+        <Grid2 display="flex" gap="2rem">
+          <TextField
+            id="number"
+            label="Number:"
+            value={formData.number}
+            onChange={handleChange}
+            required
+            type="text"
+            variant="outlined"
+          />
+
+          <TextField
+            id="provider"
+            label="Provider:"
+            value={formData.provider}
+            onChange={handleChange}
+            required
+            type="text"
+            variant="outlined"
+          />
+          <TextField
+            id="date"
+            label="Date:"
+            value={formData.date}
+            onChange={handleChange}
+            required
+            type="date"
+            variant="outlined"
+          />
+        </Grid2>
+
+        <Grid2 display="flex" gap="2rem" >
+          <TextField
+            id="totalAmount"
+            label="Total Amount:"
+            value={formData.totalAmount}
+            onChange={handleChange}
+            type="text"
+            variant="outlined"
+          />
+
+          <TextField
+            id="state"
+            label="State:"
+            value={formData.state}
+            onChange={handleChange}
+            type="text"
+            variant="outlined"
+          />
+          <Grid2 alignSelf="center" justifySelf="self-end" marginLeft="auto">
+            <Button
+              type="submit"
+              variant="contained"
+              size="medium"
+              sx={{
+                width: "160px",
+              }}
+              endIcon={<SendIcon />}
+            >
+              Send
+            </Button>
+          </Grid2>
+        </Grid2>
+      </Grid2>
     </form>
   );
 };
